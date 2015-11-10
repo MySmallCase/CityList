36dc8bf1412474d6cd61d0564a81fb2c574b7095
向云飞 <978771447@qq.com>
2015-11-10 11:26:15 +0800

    增加搜索中文和首字母搜索功能

--- CityList/CityList/CityList/CityListController.m
+++ CityList/CityList/CityList/CityListController.m
@@ -11,17 +11,21 @@
 #import "Cities.h"
 #import "CityLists.h"
 #import "City.h"
+#import "ZYPinYinSearch.h"
+#import "PinYinForObjc.h"
 
-@interface CityListController ()<UITableViewDelegate,UITableViewDataSource>
+@interface CityListController ()<UITableViewDelegate,UITableViewDataSource,UITextFieldDelegate>
 
 @property (nonatomic,strong) UITableView *tableView;
 
-//自己添加
-@property (nonatomic,strong) NSMutableArray *keys;
 
-@property (nonatomic,strong) NSArray *citiesArray;
+@property (nonatomic,strong) NSMutableArray *keys; /**< 索引*/
 
-@property (nonatomic,strong) NSMutableArray *cities;
+@property (nonatomic,strong) NSArray *citiesArray; /**< */
+
+@property (nonatomic,strong) NSMutableArray *cities; /**< */
+
+@property (nonatomic,strong) NSMutableArray *allCities; /**< 所有的cities*/
 
 
 
@@ -31,18 +35,42 @@
 
 @property (nonatomic,strong) NSTimer *timer;
 
+//搜索框
+@property (nonatomic,strong) UIView *searchView;
+
+@property (nonatomic,strong) UIView *searchBg;
+
+@property (nonatomic,strong) UIImageView *searchBarIcon;
+
+@property (nonatomic,strong) UITextField *searchText;
+
 @end
 
 @implementation CityListController
 
+- (void)initialize{
+    self.keys = [[NSMutableArray alloc] init];
+    self.cities = [[NSMutableArray alloc] init];
+    self.allCities = [[NSMutableArray alloc] init];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    //初始化操作
+    [self initialize];
     
-    self.keys = [[NSMutableArray alloc] init];
-    self.cities = [[NSMutableArray alloc] init];
+    self.view.backgroundColor = [UIColor colorWithRed:241 / 255.0 green:241 / 255.0 blue:241 / 255.0 alpha:1.0];
     
+    //
     [self getCityData];
+    
+    //添加搜索
+    [self.view addSubview:self.searchView];
+    [self.searchView addSubview:self.searchBg];
+    [self.searchBg addSubview:self.searchBarIcon];
+    [self.searchBg addSubview:self.searchText];
+    
     
     [self.view addSubview:self.tableView];
     
@@ -60,70 +88,18 @@
     self.citiesArray = [CityLists objectArrayWithKeyValuesArray:cities.cityList];
     
     for (CityLists *list in self.citiesArray) {
-        
         NSArray *array = [City objectArrayWithKeyValuesArray:list.city];
-        
-//        NSLog(@"%@",array);
-        
         [self.cities addObject:array];
-        
         [self.keys addObject:list.key];
     }
     
-//    NSLog(@"%@",self.cities);
-    
-    
-}
-
-NSInteger cityNameSort(id str1, id str2, void *context)
-{
-    NSString *string1 = (NSString*)str1;
-    NSString *string2 = (NSString*)str2;
-    
-    return  [string1 localizedCompare:string2];
-}
-
-#pragma mark - getter and setter
-- (UITableView *)tableView{
-    if (!_tableView) {
-        _tableView = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStylePlain];
-        _tableView.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height);
-//        _tableView.backgroundColor = [UIColor clearColor];
-        _tableView.rowHeight = 44.0f;
-        _tableView.dataSource = self;
-        _tableView.delegate = self;
+    for (int i = 0; i < self.cities.count; i ++) {
+        NSArray *array = [City objectArrayWithKeyValuesArray:self.cities[i]];
+        for (City *city in array) {
+            [self.allCities addObject:city.city_name];
+        }
     }
-    return _tableView;
 }
-
-
-- (UIView *)tipsView{
-    if (!_tipsView) {
-        _tipsView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 80, 80)];
-        _tipsView.backgroundColor = [UIColor blackColor];
-        _tipsView.alpha = 0.2;
-        _tipsView.layer.masksToBounds = YES;
-        _tipsView.layer.cornerRadius = 15;
-        _tipsView.layer.borderColor = [UIColor whiteColor].CGColor;
-        _tipsView.layer.borderWidth = 2;
-    }
-    return _tipsView;
-}
-
-
-- (UILabel *)tipsLabel{
-    if (!_tipsLabel) {
-        _tipsLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, self.tipsView.frame.size.width, self.tipsView.frame.size.height)];
-        _tipsLabel.backgroundColor = [UIColor clearColor];
-        _tipsLabel.font = [UIFont boldSystemFontOfSize:50.0f];
-        _tipsLabel.textAlignment = NSTextAlignmentCenter;
-        _tipsLabel.textColor = [UIColor colorWithRed:132/155.0 green:132/155.0 blue:132/155.0 alpha:1.0f];
-    }
-    return _tipsLabel;
-}
-
-
-
 
 #pragma mark - tableView
 
@@ -134,6 +110,7 @@ NSInteger cityNameSort(id str1, id str2, void *context)
 
 //自定义每个section中header的view
 - (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section{
+
     UIView *bgView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, tableView.frame.size.width, 28)];
     bgView.backgroundColor = [UIColor colorWithRed:241/255.0 green:241/255.0 blue:241/255.0 alpha:1.0f];
     
@@ -143,7 +120,7 @@ NSInteger cityNameSort(id str1, id str2, void *context)
     headerTitleLabel.font = [UIFont systemFontOfSize:12.0f];
     
     UIView *line = [[UIView alloc] initWithFrame:CGRectMake(0, 27, tableView.frame.size.width, 1)];
-    line.backgroundColor = [UIColor colorWithRed:210/255.0 green:210/255.0 blue:210/255.0 alpha:1.0];
+    line.backgroundColor = [UIColor colorWithRed:210/255.0 green:210/255.0 blue:210/255.0 alpha:0.2];
     
     NSString *key = [self.keys objectAtIndex:section];
     headerTitleLabel.text = key;
@@ -161,7 +138,7 @@ NSInteger cityNameSort(id str1, id str2, void *context)
 
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
-    return self.citiesArray.count;
+    return self.keys.count;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView sectionForSectionIndexTitle:(NSString *)title atIndex:(NSInteger)index{
@@ -202,34 +179,189 @@ NSInteger cityNameSort(id str1, id str2, void *context)
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     static NSString *ID = @"cell";
-    
-    NSArray *array = [City objectArrayWithKeyValuesArray:self.cities[indexPath.section]];
-    City *city = [City objectWithKeyValues:array[indexPath.row]];
+    NSString *cityName;
+    if (self.searchText.text.length > 0) {
+        NSArray *array = self.cities[indexPath.section];
+        cityName = array[indexPath.row];
+    }else{
+        NSArray *array = [City objectArrayWithKeyValuesArray:self.cities[indexPath.section]];
+        City *city = [City objectWithKeyValues:array[indexPath.row]];
+        cityName = city.city_name;
+    }
     
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:ID];
     if (!cell) {
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:ID];
-        cell.backgroundColor = [UIColor clearColor];
-        cell.contentView.backgroundColor = [UIColor clearColor];
         cell.selectionStyle = UITableViewCellSelectionStyleGray;
         cell.textLabel.textColor = [UIColor colorWithRed:34/255.0 green:34/255.0 blue:34/255.0 alpha:0.8f];
         cell.textLabel.font = [UIFont systemFontOfSize:13.0f];
     }
     
-    cell.textLabel.text = city.city_name;
+    cell.textLabel.text = cityName;
     return cell;
     
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
     [self dismissViewControllerAnimated:YES completion:nil];
-    
-    NSArray *array = [City objectArrayWithKeyValuesArray:self.cities[indexPath.section]];
-    City *city = [City objectWithKeyValues:array[indexPath.row]];
+    NSString *cityName;
+    if (self.searchText.text.length > 0) {
+        NSArray *array = self.cities[indexPath.section];
+        cityName = array[indexPath.row];
+    }else{
+        NSArray *array = [City objectArrayWithKeyValuesArray:self.cities[indexPath.section]];
+        City *city = [City objectWithKeyValues:array[indexPath.row]];
+        cityName = city.city_name;
+    }
     if ([self.delegate respondsToSelector:@selector(didClickedWithCityName:)]) {
-        [self.delegate didClickedWithCityName:city.city_name];
+        [self.delegate didClickedWithCityName:cityName];
     }
     
+}
+
+
+
+#pragma mark - UITextField editing change method
+- (void)textChange:(UITextField *)textField{
+    [self filterContentForSearchText:textField.text];
+}
+
+#pragma mark - 通过搜索条件过滤得到搜索结果
+- (void)filterContentForSearchText:(NSString *)searchText{
+    if (searchText.length > 0) {
+        
+        //去除原有的值
+        [self.keys removeAllObjects];
+        [self.cities removeAllObjects];
+        
+        NSMutableArray *resultArray = [[ZYPinYinSearch searchWithOriginalArray:self.allCities andSearchText:searchText andSearchByPropertyName:@""] mutableCopy];
+        for (NSString *city in resultArray) {
+            NSString *pinYinHead = [PinYinForObjc chineseConvertToPinYinHead:city].uppercaseString;
+            NSString *firstHeadPinYin = [pinYinHead substringToIndex:1];
+            [self.keys addObject:firstHeadPinYin];
+        }
+        
+        NSSet *set = [NSSet setWithArray:self.keys];
+        self.keys = [[[set allObjects] sortedArrayUsingSelector:@selector(compare:)] mutableCopy];
+        
+        //得到分组城市
+        NSMutableArray *array = [[NSMutableArray alloc] init];
+        for (int i = 0; i < self.keys.count; i ++) {
+            NSString *key = self.keys[i];
+            for (NSString *city in resultArray) {
+                NSString *pinYinHead = [PinYinForObjc chineseConvertToPinYinHead:city].uppercaseString;
+                NSString *firstHeadPinYin = [pinYinHead substringToIndex:1];
+                if ([key isEqualToString:firstHeadPinYin]) {
+                    [array addObject:city];
+                }
+            }
+            
+            [self.cities addObject:[array mutableCopy]];
+            [array removeAllObjects];
+        }
+        [resultArray removeAllObjects];
+        [self.tableView reloadData];
+        self.tableView.tableFooterView = [[UIView alloc] init];
+        
+    }else{
+        [self.keys removeAllObjects];
+        [self.cities removeAllObjects];
+        [self.allCities removeAllObjects];
+        [self getCityData];
+        [self.tableView reloadData];
+    }
+}
+
+
+#pragma mark - getter and setter
+- (UITableView *)tableView{
+    if (!_tableView) {
+        _tableView = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStylePlain];
+        _tableView.frame = CGRectMake(0, self.searchView.frame.size.height, self.view.frame.size.width, self.view.frame.size.height - self.searchView.frame.size.height);
+        _tableView.backgroundColor = [UIColor clearColor];
+        _tableView.rowHeight = 44.0f;
+        _tableView.dataSource = self;
+        _tableView.delegate = self;
+        _tableView.sectionIndexColor = [UIColor colorWithRed:34/255.0 green:34/255.0 blue:34/255.0 alpha:0.4];
+        _tableView.sectionIndexBackgroundColor = [UIColor clearColor];
+    }
+    return _tableView;
+}
+
+
+- (UIView *)tipsView{
+    if (!_tipsView) {
+        _tipsView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 80, 80)];
+        _tipsView.backgroundColor = [UIColor blackColor];
+        _tipsView.alpha = 0.2;
+        _tipsView.layer.masksToBounds = YES;
+        _tipsView.layer.cornerRadius = 15;
+        _tipsView.layer.borderColor = [UIColor whiteColor].CGColor;
+        _tipsView.layer.borderWidth = 2;
+    }
+    return _tipsView;
+}
+
+
+- (UILabel *)tipsLabel{
+    if (!_tipsLabel) {
+        _tipsLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, self.tipsView.frame.size.width, self.tipsView.frame.size.height)];
+        _tipsLabel.backgroundColor = [UIColor clearColor];
+        _tipsLabel.font = [UIFont boldSystemFontOfSize:50.0f];
+        _tipsLabel.textAlignment = NSTextAlignmentCenter;
+        _tipsLabel.textColor = [UIColor colorWithRed:132/155.0 green:132/155.0 blue:132/155.0 alpha:1.0f];
+    }
+    return _tipsLabel;
+}
+
+
+- (UIView *)searchView{
+    if (!_searchView) {
+        _searchView = [[UIView alloc] init];
+        _searchView.frame = CGRectMake(0, 0, self.view.frame.size.width, 44);
+        _searchView.backgroundColor = [UIColor colorWithRed:241 / 255.0 green:241 / 255.0 blue:241 / 255.0 alpha:1.0f];
+        
+    }
+    return _searchView;
+}
+
+
+- (UIView *)searchBg{
+    if (!_searchBg) {
+        _searchBg = [[UIView alloc] init];
+        _searchBg.frame = CGRectMake(10, 7, self.searchView.frame.size.width - 20, self.searchView.frame.size.height - 14);
+        _searchBg.backgroundColor = [UIColor whiteColor];
+        _searchBg.layer.borderColor = [UIColor colorWithRed:210 / 255.0 green:210 / 255.0 blue:210 / 255.0 alpha:1.0f].CGColor;
+        _searchBg.layer.borderWidth = 1.0f;
+        _searchBg.layer.masksToBounds = YES;
+        _searchBg.layer.cornerRadius = 15.0f;
+    }
+    return _searchBg;
+}
+
+
+- (UIImageView *)searchBarIcon{
+    if (!_searchBarIcon) {
+        _searchBarIcon = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"searchbar_textfield_search_icon"]];
+        CGFloat y = self.searchBg.frame.size.height - _searchBarIcon.image.size.height;
+        _searchBarIcon.frame = CGRectMake(5, y / 2, _searchBarIcon.image.size.width, _searchBarIcon.image.size.height);
+    }
+    return _searchBarIcon;
+}
+
+- (UITextField *)searchText{
+    if (!_searchText) {
+        _searchText = [[UITextField alloc] init];
+        _searchText.frame = CGRectMake(self.searchBarIcon.frame.size.width + self.searchBarIcon.frame.origin.x + 3, self.searchBarIcon.frame.origin.y, self.searchBg.frame.size.width - self.searchBarIcon.frame.size.width * 2 - 6, self.searchBarIcon.frame.size.height);
+        _searchText.font = [UIFont systemFontOfSize:12.0f];
+        _searchText.placeholder = @"请输入城市名称或者首字母查询";
+        _searchText.returnKeyType = UIReturnKeySearch;
+        _searchText.textColor = [UIColor colorWithRed:204/255.0 green:204/255.0 blue:204/255.0 alpha:1.0f];
+        _searchText.tintColor = [UIColor colorWithRed:204/255.0 green:204/255.0 blue:204/255.0 alpha:1.0f];
+        _searchText.delegate = self;
+        [_searchText addTarget:self action:@selector(textChange:) forControlEvents:UIControlEventEditingChanged];
+    }
+    return _searchText;
 }
 
 
